d495bd18654c00e2adbf1a5e285022a2402e02d3
osantamaria <osantamaria@gmail.com>
2022-01-01 23:59:26 -0600

    Adding security configuration

--- eazybank/src/main/java/com/eazybank/config/ProjectSecurityConfig.java
+++ eazybank/src/main/java/com/eazybank/config/ProjectSecurityConfig.java
@@ -7,11 +7,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /*
+        Requirement
+
+            /myAccount - Secured
+            /myBalance - Secured
+            /myLoans - Secured
+            /myCards - Secured
+            /notices - Not Secured
+            /contact - Not Secured
+    */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                    .antMatchers("/myAccount").authenticated()
+                    .antMatchers("/myBalance").authenticated()
+                    .antMatchers("/myLoans").authenticated()
+                    .antMatchers("/myCards").authenticated()
+                    .antMatchers("/notices").permitAll()
+                    .antMatchers("/contact").permitAll()
                 .and()
                 .formLogin()
                 .and()
