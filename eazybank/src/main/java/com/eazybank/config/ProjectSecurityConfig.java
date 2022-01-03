004fb44bad6e3e428f6991e267bf57a68183e5cb
osantamaria <osantamaria@gmail.com>
2022-01-02 20:42:41 -0600

    Defining and managing users

--- eazybank/src/main/java/com/eazybank/config/ProjectSecurityConfig.java
+++ eazybank/src/main/java/com/eazybank/config/ProjectSecurityConfig.java
@@ -1,11 +1,23 @@
 package com.eazybank.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
     /*
         Requirement
